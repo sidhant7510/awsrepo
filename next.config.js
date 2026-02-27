228110aa7c99bbce6f8bcc3994866f773bc3d109
sidhant372 <sidh372@gmail.com>
2026-02-27 19:17:43 +0530

    Update nginx template to include upgrade headers and restart service

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  reactStrictMode: true,
+  poweredByHeader: false,
+  compress: true,
+};
+
+module.exports = nextConfig;
